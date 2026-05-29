dbc15342b09965d22254f365a6c50c1754adb539
venkatruth <venkat.ruth@gmail.com>
2026-05-29 18:48:36 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,17 @@
-self.addEventListener("install", e=>{
- e.waitUntil(
-  caches.open("app").then(c=>c.addAll([
-   "./","index.html","manifest.json","icon.png"
-  ]))
- );
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("math-app").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./icon.png"
+      ]);
+    })
+  );
 });
 
-self.addEventListener("fetch", e=>{
- e.respondWith(
-  caches.match(e.request).then(r=>r||fetch(e.request))
- );
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
